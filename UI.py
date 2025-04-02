ff2203f65df10955f804fc72ba86277126ee373c
BenjaminWray2008 <benjamin.wray2008@gmail.com>
2025-04-03 12:39:38 +1300

    Made all the tables in the database so program could run, then fixed all the errors when running it

--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 import sqlite3
 from werkzeug.security import check_password_hash
+from hashlib import sha256
 
 with sqlite3.connect("times.db",check_same_thread=False) as database: #Connecting the database
     db=database.cursor()
@@ -17,8 +18,17 @@ with sqlite3.connect("times.db",check_same_thread=False) as database: #Connectin
     
     @app.route('/new_user', methods=['POST'])
     def new_user():
-        user_name='thing requested from the form'
-        user_password='other thing requested from the form'
+        username = request.form.get('username')
+        password = request.form.get('password')
+       
+        h = sha256()
+        h.update(password.encode())
+        hash = h.hexdigest()
+        db.execute('INSERT INTO User (name, hash) VALUES (?, ?)', (username, hash))
+        # db.execute('INSERT INTO Run ()')
+        # Make all the empty entries in Run table
+        database.commit()
+        
         return render_template('home.html')
     
     @app.route('/search', methods=['POST'])
@@ -29,10 +39,13 @@ with sqlite3.connect("times.db",check_same_thread=False) as database: #Connectin
         db.execute(f'SELECT id, hash FROM User WHERE name = "{username}";')
         results=db.fetchone()
         if results:
-
+            h = sha256()
+            h.update(password.encode())
+            hashed = h.hexdigest()
             user_id, hash = results
+            if hash == hashed:
            
-            if check_password_hash(hash, password):
+            
                 return redirect(url_for('get_times',user_id=user_id, category_id=1))
 
             return render_template('profile.html', user_id = user_id, category_id = 1)
@@ -41,16 +54,16 @@ with sqlite3.connect("times.db",check_same_thread=False) as database: #Connectin
         return render_template('home.html')
 
     @app.route('/get_times/<int:user_id>/<int:category_id>', methods=['GET','POST'])
-    def input_times(user_id,category_id):
+    def get_times(user_id,category_id):
         checkpoints = []
-        db.execute('SELECT time, run_number FROM Runs WHERE id = ? AND category_id = ?', (user_id, category_id))
+        db.execute('SELECT time, run_number FROM Run WHERE id = ? AND category_id = ?', (user_id, category_id))
         times = [row[0] for row in db.fetchall()]
         checkpoint_id = [row[1] for row in db.fetchall()]
         for checkpoint in checkpoint_id:
             db.execute('SELECT name FROM checkpoint WHERE id = ?', (checkpoint,))
             checkpoints.append(db.fetchall())
-        db.execute('SELECT name, count FROM categoty WHERE id = ?', (category_id,))
-        name, count = db.fetchall()
+        db.execute('SELECT name, count FROM category WHERE id = ?', (category_id,))
+        name, count = db.fetchall()[0]
         return render_template('get_times.html', user_id = user_id, category_id = category_id, times = times, checkpoints = checkpoints, count = count, name = name)
 
     
@@ -66,7 +79,7 @@ with sqlite3.connect("times.db",check_same_thread=False) as database: #Connectin
     @app.route('/profile/<int:user_id>/<int:category_id>', methods=['GET','POST'])
     def profile(user_id, category_id):
         checkpoints = []
-        db.execute('SELECT time, run_number FROM Runs WHERE id = ? AND category_id = ?', (user_id, category_id))
+        db.execute('SELECT time, run_number FROM Run WHERE id = ? AND category_id = ?', (user_id, category_id))
         times = [row[0] for row in db.fetchall()]
         checkpoint_id = [row[1] for row in db.fetchall()]
         for checkpoint in checkpoint_id:
