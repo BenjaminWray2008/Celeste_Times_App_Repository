c75437e1df9cdf7b363bfd771b53e4a4772a8492
BenjaminWray2008 <benjamin.wray2008@gmail.com>
2025-08-25 12:58:40 +1200

    formatting to pep8

--- UI.py
+++ UI.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, session, jsonify, abort, request, g
 import sqlite3
-from werkzeug.security import check_password_hash
 from werkzeug.utils import secure_filename
 from hashlib import sha256
 import re
@@ -8,57 +7,78 @@ from math import trunc
 import datetime
 from dateutil.relativedelta import relativedelta
 import os
-import copy
 
-#Globals
+# Globals
+
 listy = ['hi', 'Any%', 'ARB', '100%', 'True Ending', 'Bny%', 'Cny%']
 another_listy = [0.001, 1, 60, 3600]
 
-with sqlite3.connect("times.db",check_same_thread=False) as database: #Connecting the database
-    db=database.cursor()
-    app=Flask(__name__)
+with sqlite3.connect("times.db", check_same_thread=False) as database:
+    db = database.cursor()
+    app = Flask(__name__)
     app.secret_key = 'password'
 
-    def time_clause(thing):
+    def time_clause(thing):  # Time words plural/singular e.g. 1 year, 2 years
         if thing == 1:
             return ''
         else:
             return 's'
 
     def sob_adder(user_id):
-        sob_dict = {'Category':(('Time:', '#'),('Time:', '#')),'Any%':[], 'ARB': [], '100%': [], 'True Ending': [], 'Bny%': [], 'Cny%': []}
+        sob_dict = {'Category': (('Time:', '#'), ('Time:', '#')),
+                    'Any%': [], 'ARB': [], '100%': [], 'True Ending': [],
+                    'Bny%': [], 'Cny%': []}
+        
         for category in list(sob_dict.keys())[1:]:
-            db.execute('SELECT SUM(time) AS sob FROM Run WHERE user_id = ? AND type = "checkpoint" AND category_id = (SELECT id FROM Category WHERE name = ?);', (user_id, category))
+            db.execute('''SELECT SUM(time) AS sob FROM Run WHERE user_id = ?
+                       AND type = "checkpoint"
+                       AND category_id = (
+                           SELECT id FROM Category WHERE name = ?);''',
+                       (user_id, category))
+            
             sob = db.fetchone()[0]
             db.execute('SELECT id FROM Category WHERE name = ?', (category,))
-            category_id = db.fetchone()[0] 
-            sob_leaderboard = ranker('ORDER by sum_of_bests ASC', category_id, 'AND r1.type = "checkpoint"')
+            category_id = db.fetchone()[0]
+            
+            sob_leaderboard = ranker('ORDER by sum_of_bests ASC', category_id,
+                                     'AND r1.type = "checkpoint"')
+            
             print(sob_leaderboard)
             for time in sob_leaderboard:
                 
                 if (time[2] == sob) and (user_id == time[0]):
-                    sob_dict[category].append((format_time_readable_form(format_time_normal_form(sob)), prefix_adder(sob_leaderboard.index(time)+1)))
+                    sob_dict[category].append((
+                        format_time_readable_form(
+                            format_time_normal_form(sob)),
+                        prefix_adder(sob_leaderboard.index(time)+1)))
            
             if not sob_dict[category]:
-                sob_dict[category].append((format_time_readable_form(format_time_normal_form(sob)), 'N/A'))
+                sob_dict[category].append((
+                    format_time_readable_form(format_time_normal_form(sob)),
+                    'N/A'))
 
-
-            db.execute('SELECT SUM(time) AS sob FROM Run WHERE user_id = ? AND type = "IL" AND category_id = ?', (user_id, category_id))
+            db.execute('''SELECT SUM(time) AS sob FROM Run WHERE user_id = ?
+                       AND type = "IL"
+                       AND category_id = ?''', (user_id, category_id))
             sob = db.fetchone()[0]
         
-            sob_leaderboard = ranker('ORDER by sum_of_bests ASC', category_id, 'AND r1.type = "IL"')
+            sob_leaderboard = ranker('ORDER by sum_of_bests ASC', category_id,
+                                     'AND r1.type = "IL"')
            
             for time in sob_leaderboard:
                 
                 if (time[2] == sob) and (user_id == time[0]):
-                    sob_dict[category].append((format_time_readable_form(format_time_normal_form(sob)), prefix_adder(sob_leaderboard.index(time)+1)))
+                    sob_dict[category].append((
+                        format_time_readable_form(
+                            format_time_normal_form(sob)),
+                        prefix_adder(sob_leaderboard.index(time)+1)))
   
             if len(sob_dict[category]) < 2:
-                sob_dict[category].append((format_time_readable_form(format_time_normal_form(sob)), 'N/A'))
-
-
-        
-
+                sob_dict[category].append((
+                    format_time_readable_form(
+                        format_time_normal_form(sob)),
+                    'N/A'))
+                
         print(sob_dict)
         return sob_dict
     
@@ -81,17 +101,13 @@ with sqlite3.connect("times.db",check_same_thread=False) as database: #Connectin
     
         if float(time) != 0:
             minutes = float(time)//60
-            seconds = float(time)%60
+            seconds = float(time) % 60
             
-           
-            milliseconds = float(time)-trunc(float(time))
-         
+            milliseconds = float(time) - trunc(float(time))
             milliseconds = round(milliseconds, 3)
- 
             milliseconds = str(milliseconds)[2:]
-
-     
             time = f"{int(minutes)}:{int(seconds)}.{milliseconds}"
+            
         else:
             time = 0
 
@@ -114,37 +130,33 @@ with sqlite3.connect("times.db",check_same_thread=False) as database: #Connectin
             if len(str(ms)) < 3:
                 length = len(str(ms))
                 ms = f'{ms}{"0"*(3-length)}'
+                
             time = f'{minutes}:{seconds}.{ms}'
             
-        return time  
+        return time
     
     def format_time_second_form(time):
-    
-        total=0
-        total_individual_time=0
+        total = 0
+        total_individual_time = 0
         time_list = re.split('[.:]', str(time))
- 
         backwards_time_list = reversed(time_list)
     
         for index, time_segment in enumerate(backwards_time_list):
-           
             total_individual_time = 0
-    
-            total_individual_time += (float(time_segment)*float(another_listy[index]))
- 
-
+            new_time = float(time_segment)*float(another_listy[index])
+            total_individual_time += new_time
             total += total_individual_time
     
         total = int(total)
         total = str(total)
-   
-        total += '.' 
+        total += '.'
         total += time_list[-1]
        
-        return total    
+        return total
        
     def valid_time_checker(time):
         has_colon = False
+        
         for char in time:
             if str(char) not in '0123456789.:':
                 print('invalid chars')
@@ -152,133 +164,168 @@ with sqlite3.connect("times.db",check_same_thread=False) as database: #Connectin
                 
             if char == ':':
                 has_colon = True
+                
             if ':' in time:
-                if char == '.' and has_colon == False:
+                if char == '.' and not has_colon:
                     print('period before colon')
                     return False
+                
         if time.count('.') > 1 or time.count('.') < 1:
             print('more or less than 1 period')
             return False
+        
         if time.count(':') > 1:
             print('more than 1 colon')
             return False
         
-
         try:
             time_list = time.split('.')
-            before_period = time_list[0]; after_period = time_list[1]
-            if (len(str(after_period)) < 4 and len(str(after_period)) > 0) and (str(after_period).isdigit()):
+            before_period = time_list[0]
+            after_period = time_list[1]
+            len_aft_p = len(str(after_period))
+           
+            if len_aft_p < 4 and len_aft_p > 0 and str(after_period).isdigit():
                 pass
+            
             else:
                 print('more than 3 ms or none')
                 return False
+            
             print('did this')
             if ':' in time:
                 time_segments = re.split('[.:]', time)
                 print('splited')
-                minutes = time_segments[0] 
-                if (len(str(minutes)) < 3 and len(str(minutes)) > 0) and (str(minutes).isdigit()) and (int(minutes) < 60):
+                minutes = time_segments[0]
+                
+                if (len(str(minutes)) < 3 and
+                   len(str(minutes)) > 0 and
+                   str(minutes).isdigit() and int(minutes) < 60):
                     pass
+               
                 else:
                     print('more than 2 minutes or none')
                     return False
+                
                 seconds = time_segments[1]
-                if (len(str(seconds)) < 3 and len(str(seconds)) > 0) and (str(seconds).isdigit() and (int(seconds) < 60)):
+                if (len(str(seconds)) < 3 and
+                   len(str(seconds)) > 0 and
+                   str(seconds).isdigit() and int(seconds) < 60):
                     pass
+                
                 else:
                     print('more than 2 seconds or none')
                     return False
             else:
-                if (len(str(before_period)) < 3 and len(str(before_period)) > 0) and (str(before_period).isdigit() and (int(before_period) < 60)):
+                if (len(str(before_period)) < 3 and
+                   len(str(before_period)) > 0 and
+                   str(before_period).isdigit() and
+                   int(before_period) < 60):
                     pass
+                
                 else:
                     print('seconds with no minutes more than 2 or none')
                     return False
                 
             return True
+        
         except:
             return False
         
     def new_user_data(id):
         
-        db.execute('SELECT id FROM Category;') #Getting all available categories
+        db.execute('SELECT id FROM Category;')
         results = db.fetchall()
-        
         for category in results:
-
-            
-
-
             category_id = category[0]
-
-
-            
-         
-            
             chapter_listy = []
-            
-
-            db.execute('SELECT checkpoint_id FROM CategoryCheckpoint WHERE category_id = ?', (category_id,))
-            checkpoints_list = db.fetchall() #For each category, getting a list of all checkpoints in that category
+            db.execute('''SELECT checkpoint_id FROM CategoryCheckpoint
+                       WHERE category_id = ?''', (category_id,))
+            checkpoints_list = db.fetchall()
             
             for checkpoint in checkpoints_list:
-                db.execute('SELECT name FROM Checkpoint WHERE id = ?', (checkpoint[0],)) #Formatting results
+                db.execute('SELECT name FROM Checkpoint WHERE id = ?',
+                           (checkpoint[0],))
                 results = db.fetchone()
                 name = results[0]
+                
                 chapter_name = (name.split(' '))[-1]
                 if chapter_name not in chapter_listy:
                     chapter_listy.append(chapter_name)
-                db.execute('SELECT id FROM Chapter WHERE name = ?', (chapter_name,))
+                db.execute('SELECT id FROM Chapter WHERE name = ?',
+                           (chapter_name,))
                 results = db.fetchone()
                 chapter_id = results[0]
-                
-                db.execute('''INSERT INTO Run 
-                           (time, run_number, type, chapter_id, user_id, category_id)
+
+                db.execute('''INSERT INTO Run
+                           (time, run_number, type,
+                           chapter_id, user_id, category_id)
                            VALUES (0, ?, ?, ?, ?, ?)''',
-                           (checkpoint[0], 'checkpoint', chapter_id, id, category_id)) #For every checkpoint for every category, inserting a run entry for the new user.
+                           (checkpoint[0], 'checkpoint',
+                            chapter_id, id, category_id))
          
             for chapter in chapter_listy:
                 db.execute('SELECT id FROM Chapter WHERE name = ?', (chapter,))
                 chapter_id_real = db.fetchone()[0]
-                db.execute('''INSERT INTO Run 
-                            (time, run_number, type, chapter_id, user_id, category_id)
+                
+                db.execute('''INSERT INTO Run
+                            (time, run_number, type,
+                            chapter_id, user_id, category_id)
                             VALUES (0, ?, ?, ?, ?, ?)''',
-                            (-1, 'IL', chapter_id_real, id, category_id))
-            database.commit()
-
+                            (-1, 'IL', chapter_id_real,
+                             id, category_id))
             database.commit()
 
     def data_dictionary_creation(user_id, category_id, variable):
         data_dictionary = {}
         db.execute('''SELECT checkpoint_id, orderer FROM CategoryCheckpoint 
-                   WHERE category_id = ? ORDER BY orderer ASC''', (category_id,)) #Gets all checkpoints of a specific category in order of display
+                   WHERE category_id = ? ORDER BY orderer ASC''',
+                   (category_id,))
         results = db.fetchall()
         checkpoint_id = [j[0] for j in results]
 
-        db.execute('SELECT chapter_id FROM Run WHERE type = "IL" AND user_id = ? AND category_id = ?;', (user_id, category_id))
+        db.execute('''SELECT chapter_id FROM Run
+                   WHERE type = "IL"
+                   AND user_id = ? AND
+                   category_id = ?;''',
+                   (user_id, category_id))
         chapters_query = db.fetchall()
     
         data_dictionary['Chapter SOB'] = []
         placeholders = ', '.join('?' for i in chapters_query) 
-        db.execute(f'SELECT name FROM Chapter WHERE id IN ({placeholders}) ORDER BY chapter_order ASC', tuple(i[0] for i in chapters_query))
+        db.execute(f'''SELECT name FROM Chapter WHERE id IN (
+            {placeholders})
+            ORDER BY chapter_order ASC''',
+            tuple(i[0] for i in chapters_query))
+        
         new_chapters = db.fetchall()
        
-        
         for index, chapter in enumerate(new_chapters):
-        
             db.execute('SELECT id FROM Chapter WHERE name = ?', chapter)
             chapter_id = db.fetchone()[0]
-            db.execute('SELECT time FROM Run WHERE type = "IL" AND user_id = ? AND category_id = ? AND chapter_id = ?', (user_id, category_id, chapter_id))
+            
+            db.execute('''SELECT time FROM Run WHERE type = "IL"
+                       AND user_id = ? AND
+                       category_id = ? AND
+                       chapter_id = ?''',
+                       (user_id, category_id, chapter_id))
             IL_time = db.fetchone()[0]
-            data_dictionary['Chapter SOB'].append([chapter[0]+' hi', format_time_readable_form(format_time_normal_form(IL_time))])
+            
+            data_dictionary['Chapter SOB'].append([chapter[0]+' hi',
+                                                   format_time_readable_form(
+                                                       format_time_normal_form(
+                                                           IL_time))])
         
         for checkpoint in checkpoint_id:
-            db.execute('''SELECT name FROM Chapter WHERE id IN 
-                       (SELECT chapter_id FROM Run 
-                       WHERE run_number = ? AND user_id = ? AND category_id = ? AND type = "checkpoint");''', (checkpoint, user_id, category_id)) #For every checkpoint get the name of the chapter that it is in
+            db.execute('''SELECT name FROM Chapter WHERE id IN
+                       (SELECT chapter_id FROM Run
+                       WHERE run_number = ? AND
+                       user_id = ? AND
+                       category_id = ? AND
+                       type = "checkpoint");''',
+                       (checkpoint, user_id, category_id))
             results = db.fetchone()
            
-            if results[0] not in data_dictionary: #If the chapter is not already in the dictionary add it as an empty list
+            if results[0] not in data_dictionary:
                 data_dictionary[results[0]] = []
             db.execute('SELECT name FROM checkpoint WHERE id = ?', (checkpoint,)) #Get the name of the checkpoint and the time the user has for it
             checkpoint_name = db.fetchone()
